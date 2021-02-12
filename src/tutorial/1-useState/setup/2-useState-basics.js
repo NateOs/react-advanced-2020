e064fe9736a8a7d8479f81d4e09dc86b5b6a2328
nateos <sodjanathan@gmail.com>
2021-02-12 23:16:31 +0000

    UseState Impelementation

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,25 @@
 import React, { useState } from 'react';
+//use
+// component name must be uppercase
+// must be in the fxn/comp body
+// cannot call conditionally
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState('random')
+
+  const handleClick = () => {
+    if (text === 'random') {
+      setText('HELLO')
+    } else {
+      setText('RANDOM TITLE')
+    }
+  }
+  return <> 
+    <h1>{text}</h1>
+    <button className="btn" onClick={handleClick}>
+      Change title
+    </button>
+  </>
 };
 
 export default UseStateBasics;
