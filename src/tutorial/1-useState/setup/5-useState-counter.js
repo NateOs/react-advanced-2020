a83c87c6a9ebbde03d9b0f64a7cb435e7be472f4
nateos <sodjanathan@gmail.com>
2021-02-16 21:31:12 +0000

    Functional Update Form (prevents certain weird behaviours)

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -12,6 +12,15 @@ const UseStateCounter = () => {
     setValue(value - 1)
   }
 
+  const complexIncrease = () => {
+    setTimeout(() => {
+      // setValue(value + 2)
+      setValue((prevState) => { // using the functional update form can resolve certain bugs, like the unrecognised clicks here
+        return prevState + 1    // setValue works but the setTimeOut makes it work improperly
+      })
+    }, 2000)
+  }
+
   return (
     <> 
       <section style={{ margin: '4rem 0'}}>
@@ -20,6 +29,11 @@ const UseStateCounter = () => {
         <button className='btn' onClick={adder}>Add</button>
         <button className='btn' onClick={() => setValue(0)}>Reset</button>
         <button className='btn' onClick={substracter}>Minus</button>
+      </section>
+      <section style={{ margin: '4rem 0'}}>
+        <h2>more complex counter</h2>
+        <h1>{value}</h1>
+        <button className='btn' onClick={complexIncrease}>Increase later</button>
       </section>
     </>
   )
