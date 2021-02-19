66d04ec84c970c70c8cd7d37ac095790ab422878
nateos <sodjanathan@gmail.com>
2021-02-19 02:24:16 +0000

    Show or hide component conditionally

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,33 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false)
+
+  return <>
+    <h2>show/hide</h2>
+    <button className='btn' onClick={() => setShow(!show)}>Toggle</button>
+    {show && <Item />}
+  </>
 };
+
+const Item = () => {
+  const [size, setSize] = useState(window.innerWidth)
+  const checkSize = () => {
+    setSize(window.innerWidth)
+  }
+  useEffect(() => {
+    window.addEventListener('resize', checkSize)
+    return () => {
+      window.removeEventListener('resize', checkSize) //cleanup function for useEffect
+    }
+  }, []) // the empty array arg for the depemdency ele works well for elements but not components, that's why the use of the cleanUp function
+
+  return (
+    <div style={{ marginTop: '2rem'}}>
+      <h1>window</h1>
+      <h2>size :{size} px</h2>
+    </div>
+  )
+}
 
 export default ShowHide;
