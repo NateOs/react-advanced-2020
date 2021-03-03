fb4a57975763ec4fa67bf57d2e8345a091266ecb
nateos <sodjanathan@gmail.com>
2021-03-03 11:21:06 +0000

    using useContext to avoid prop drilling modified the code to pass down the removePerson func

--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -3,6 +3,9 @@ import { data } from '../../../data';
 // more components
 // fix - context api, redux (for more complex cases)
 
+const PersonContext = React.createContext()
+// two components , the Provider and Consumer
+
 const ContextAPI = () => {
   const [people, setPeople] = useState(data);
   const removePerson = (id) => {
@@ -10,10 +13,13 @@ const ContextAPI = () => {
       return people.filter((person) => person.id !== id);
     });
   };
+
   return (
     <>
-      <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
+      <PersonContext.Provider value={{removePerson}}>
+        <h3>Context API/ useContext</h3>
+        <List people={people} />
+      </PersonContext.Provider>
     </>
   );
 };
@@ -26,7 +32,6 @@ const List = ({ people, removePerson }) => {
           <SinglePerson
             key={person.id}
             {...person}
-            removePerson={removePerson}
           />
         );
       })}
@@ -34,7 +39,8 @@ const List = ({ people, removePerson }) => {
   );
 };
 
-const SinglePerson = ({ id, name, removePerson }) => {
+const SinglePerson = ({ id, name }) => {
+  const { removePerson } = useContext(PersonContext)
   return (
     <div className='item'>
       <h4>{name}</h4>
