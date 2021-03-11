32da40a0af4ab01f61ccf965578c0ee173f14fbf
nateos <sodjanathan@gmail.com>
2021-03-11 09:26:09 +0000

    Checking propTypes

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ( {image, name, price} ) => {
+  return (
+  <article className='product'>
+    {/* <img src={image.url} alt={name} />
+    <h4>{name}</h4>
+    <p>${price}</p> */}
+  </article>);
 };
+
+
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.object.isRequired
+}
+
 
 export default Product;
